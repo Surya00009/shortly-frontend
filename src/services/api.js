f5e820eb84ec0b56b388e0b4787d86af7620b868
surya <suryaprakashnaidu77@gmail.com>
2026-07-08 15:46:58 +0530

    Use environment variables for API configuration

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
 
-    baseURL:"http://localhost:8080"
-
+    baseURL: import.meta.env.VITE_API_URL
 });
+console.log("API URL =", import.meta.env.VITE_API_URL);
 
 api.interceptors.request.use(config=>{
 
